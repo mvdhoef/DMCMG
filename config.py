625403b342f557d20ec221d4f9d4406f94adcf53
Mattias van de Hoef <mvdhoef@gmail.com>
2016-03-20 15:27:04 -0500

    adding overviewer config file

---
+++ config.py
@@ -0,0 +1,11 @@
+# This is a sample config file, meant to give you an idea of how to format your
+# config file and what's possible.
+worlds["My World"]="/mcserver/world"
+renders["Simple"] = {
+    "world": "My World",
+    "title": "Survival Daytime",
+    "rendermode": smooth_lighting,
+    "dimension": "overworld",
+}
+texturepath="/data/1.9.jar"
+outputdir="/map"
